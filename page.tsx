c5eb95af44f5450b60db36b37a92f56f5a883317
Jahansherkhan <160265854+jahansher333@users.noreply.github.com>
2024-09-06 00:25:38 +0500

    Hello World

---
+++ page.tsx
@@ -0,0 +1,7 @@
+export default function () {
+  return(
+    <div className=" text-balance bg-black text-white  width-100 height-10 p-10 text-3xl font-bold text-center">
+      <h1>Hello World</h1>
+    </div>
+  )
+}
